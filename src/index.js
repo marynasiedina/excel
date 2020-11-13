79f6f1a3df0f11698d819cadbd7806707cc682d5
marynasiedina <marynasiedina@gmail.com>
2020-11-13 15:59:27 +0200

    finished css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-
 import './scss/index.scss'
-const heading = document.createElement('h1')
-heading.textContent = 'Как интересно!!!!!'
-
-const root = document.querySelector('#app')
-root.append(heading)
-
-console.log('working')
