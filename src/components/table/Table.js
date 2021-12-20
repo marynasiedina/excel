86dcea1e9dc2689186fafb6f8805ed124e5794ee
marynasiedina <marynasiedina@gmail.com>
2021-12-20 20:57:11 +0200

    perfomane optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,37 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
 import { createTable } from './table.template';
+import { $ } from '../../core/dom'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
   toHTML() {
     return createTable(20)
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target)
+      console.log('mouse')
+      // const $parent = $resizer.$el.parentNode // bad!
+      // const $parent = $resizer.$el.closest('.column') // better but bad!
+      const $parent = $resizer.closest('[data-type="resizable"]')
+      const coords = $parent.getCoords()
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+
+      document.onmousemove = e => {
+        const delta = e.pageX - coords.right
+        const value = coords.width + delta
+        $parent.$el.style.width = value + 'px'
+        cells.forEach( el => el.style.width= value + 'px')
+      }
+      document.onmouseup = () => {
+        document.onmousemove = null
+      }
+    }
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,22 +3,32 @@ const CODES = {
   Z: 90
 }
 
-function toCell() {
-  return `<div class="cell" contenteditable></div>`
+function toCell( _, col ) {
+  return `<div class="cell" data-col="${col}" contenteditable></div>`
 }
 
-function toColumn(col) {
+function toColumn( col, index ) {
   return `
-  <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
   `
 }
 
 function createRow(index, content) {
+  const resizer = index
+    ? '<div class="row-resize" data-resize="row"></div>'
+    : ''
+
   return `
-  <div class="row">
-    <div class="row-info">${index ? index : ''}</div>
-    <div class="row-data">${content}</div>
-  </div>`
+    <div class="row">
+      <div class="row-info">${index ? index : ''}
+        ${resizer}
+      </div>
+      <div class="row-data">${content}</div>
+    </div>
+  `
 }
 
 function toChar(_, index) {
@@ -29,20 +39,19 @@ export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
   const cols = new Array(colsCount)
-    .fill('')
-    .map(toChar)
-    .map(toColumn) // = .map(el => { createCol(el) })
-    .join('')
-
+      .fill('')
+      .map(toChar)
+      .map(toColumn) // = .map(el => { createCol(el) })
+      .join('')
 
   rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount)
-      .fill('')
-      .map(toCell)
-      .join('')
-
+        .fill('')
+        .map(toCell)
+        .join('')
+    
     rows.push(createRow(i + 1, cells))
   }
 
