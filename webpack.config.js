a81408509a7da7b707c618cc945095917f71f2da
marynasiedina <marynasiedina@gmail.com>
2022-02-01 13:37:54 +0200

    created unit testing env

--- webpack.config.js
+++ webpack.config.js
@@ -67,11 +67,11 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime',
-              '@babel/plugin-proposal-class-properties']
-          }
+          // options: {
+          //   presets: ['@babel/preset-env'],
+          //   plugins: ['@babel/plugin-transform-runtime',
+          //     '@babel/plugin-proposal-class-properties']
+          // }
         },
       },
       // CSS, SASS
